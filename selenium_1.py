6bf7c11386efb7905ad1f68e0e2c3437f7dc61f4
V S Sreejeet <31929912+sreejeet@users.noreply.github.com>
2019-02-06 23:06:06 +0530

    Better like engine

--- selenium_1.py
+++ selenium_1.py
@@ -1,29 +1,35 @@
 '''
 A basic selenium script to automateliking posts on your instagram feed
-This program was created as part of my selenium learning process. I will not be
-held responsible for any misuse of this code.
+It will like all posts, there is no filter in place; you have been warned.
+I created this script as part of my selenium learning process and will not
+be held responsible for any misuse of this code.
+Author: V S Sreejeet
 '''
+
 
 import time
 import sys
 import selenium
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 
+
+# Time format for output log
+def t():
+    return time.strftime('[%A %H:%M:%S] ')
 
 # User setup
 username = 'username'
 password = 'password'
 
 
-# Bot setup
-print('Setting up bot...')
-bot         = webdriver.Firefox()
-bot.implicitly_wait(5)
-# bot.set_page_load_timeout(10)
-errs        = 0
-b_page_height = 0
-a_page_height = 1
-liked       = 0
+# driver setup
+print(t() + 'Setting up driver...')
+driver = webdriver.Firefox()
+driver.implicitly_wait(5)
+errs = 0
+liked = 0
 try:
     to_like = int(sys.argv[1])
 except IndexError as e:
@@ -31,156 +37,134 @@ except IndexError as e:
 try:
     step_liked = int(sys.argv[2])
 except IndexError as e:
-    step_liked = 5
+    step_liked = 50
 try:
-    max_retries = int(sys.argv[3])
+    max_errors = int(sys.argv[3])
 except IndexError as e:
-    max_retries = 10
+    max_errors = 10
 
-def click_element(browser, element, tryNum=0):
-    try:
-        element.click()
-    except Exception:
-        if tryNum == 0:
-            # try scrolling the element into view
-            browser.execute_script(
-                "document.getElementsByClassName('" + element.get_attribute(
-                    "class") + "')[0].scrollIntoView({ inline: 'center' });")
-        elif tryNum == 1:
-            # well, that didn't work, try scrolling to the top and then
-            # clicking again
-            browser.execute_script("window.scrollTo(0,0);")
-        elif tryNum == 2:
-            # that didn't work either, try scrolling to the bottom and then
-            # clicking again
-            browser.execute_script(
-                "window.scrollTo(0,document.body.scrollHeight);")
-        else:
-            # try `execute_script` as a last resort
-            # print("attempting last ditch effort for click, `execute_script`")
-            browser.execute_script(
-                "document.getElementsByClassName('" + element.get_attribute(
-                    "class") + "')[0].click()")
-            # end condition for the recursive function
-            return
-        tryNum += 1
-        # try again!
-        click_element(browser, element, tryNum)
 
 # Logging in
-print('Logging in as %s...' % (username))
+print(t() + 'Logging in as %s...' % (username))
 tries = 0
 got = False
 while(not got):
     tries += 1
-
     if tries > 5:
-        print('Unable to login after 5 tries!!\nExiting...')
+        print(t() + 'Unable to login after 5 tries!!\nExiting...')
         quit()
-
     time.sleep(2)
     try:
-        bot.get('https://instagram.com/accounts/login')
+        driver.get('https://instagram.com/accounts/login')
         # time.sleep(4)
-        bot.find_element_by_name('username').send_keys(username)
-        bot.find_element_by_name('password').send_keys(password)
-        bot.find_element_by_class_name("_0mzm-.sqdOP.L3NKy").click()
+        driver.find_element_by_name('username').send_keys(username)
+        driver.find_element_by_name('password').send_keys(password)
+        driver.find_element_by_class_name("_0mzm-.sqdOP.L3NKy").click()
         got = True
     except selenium.common.exceptions.WebDriverException as e:
         print(str(e))
         time.sleep(3)
-        bot.refresh()
+        driver.refresh()
 
 time.sleep(2)
 try:
-    bot.find_element_by_class_name('aOOlW.HoLwm').click()
-    print('Logged in successfully')
+    # driver.find_element_by_class_name('aOOlW.HoLwm').click()
+    print(t() + 'Logged in successfully')
 except Exception as e:
     print(e)
-    print('Something\'s wrong!')
+    print(t() + 'Something\'s wrong!')
     print('Waiting for manual log in.')
     input('Enter to continue...')
 finally:
-    time.sleep(1)
+    time.sleep(2)
 
 try:
     # Turn on notifications? No
-    bot.find_element_by_class_name('aOOlW.HoLwm').click()
+    driver.find_element_by_class_name('aOOlW.HoLwm').click()
 except selenium.common.exceptions.NoSuchElementException:
-    # Sometimes this alert is missing, So nothing to do here
+    # Sometimes this alert is missing
     pass
 
-# like engine
-print('Going to like %d posts...' % (to_like))
-print('Will notify at every %d likes...' % (step_liked))
+
+# Main like engine
+print(t() + 'Going to like %d posts' % (to_like))
+print(t() + 'Will notify at every %d likes' % (step_liked))
 
 tmp = step_liked
-a_page_height = int(bot.execute_script('return document.body.scrollHeight'))
+delay = 5 # delay for each like in seconds
+scrolled = 0
+refreshed = 0
+btn = None
+
 try:
     while(liked < to_like):
-        # Stop if too many errors
-        if errs > max_retries:
-            print('Too many errors!!')
+        if errs > max_errors:
+            # Stop if too many errors
+            print(t() + 'Too many errors!!')
             break
 
-        like_list = bot.find_elements_by_class_name(\
-            'glyphsSpriteHeart__outline__24__grey_9.u-__7')
-        like_list = [x for x in like_list if x.get_attribute('aria-label')=='Like']
-
-        # Nothing to like at this position, scroll ahead
-        if len(like_list) < 1:
-            # print('Nothing to like here, moving forward.')
-            b_page_height = int(bot.execute_script('return document.body.scrollHeight'))
-            bot.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(3)
-            a_page_height = int(bot.execute_script('return document.body.scrollHeight'))
-
-            if not b_page_height < a_page_height:
-                # Average page max height = 1118000
-                print(b_page_height, a_page_height)
-                # Infinite scroll exhausted, refresh and wait
-                print('Page exhausted. Refreshing...')
-                bot.refresh()
-                time.sleep(2)
-            continue
-
         try:
-            # for x in [x for x in like_list if x.get_attribute('aria-label')=='Like']:
-            for like_btn in like_list:
-                bot.execute_script( "document.getElementsByClassName('"\
-                    + like_btn.get_attribute("class")\
-                    + "')[0].scrollIntoView({ inline: 'center' });")
-                click_element(bot, like_btn)
+            # Avoid that button on the top bar by only selecting span elements
+            # with aria-label == 'like'
+            like_list = driver.find_elements_by_class_name(\
+                'glyphsSpriteHeart__outline__24__grey_9.u-__7')
+            like_list = [x for x in like_list\
+                if x.get_attribute('aria-label') == 'Like']
+
+            if len(like_list) < 1:
+                # No posts to like, scrolling ahead
+                print('Scrolled down %d times without anything to like.' % scrolled)
+                driver.execute_script(\
+                    "window.scrollTo(0, document.body.scrollHeight);")
+                scrolled += 1
+                time.sleep(3)
+                height = int(\
+                    driver.execute_script('return document.body.scrollHeight'))
+            
+                if scrolled > 20 or height > 1000000:
+                    # Scrolled far but still no posts to like, refresh page
+                    if refreshed > 5:
+                        print(t() + 'Nothing to like at the moment.')
+                        break
+                    print('Refreshing page at height', height)
+                    driver.refresh()
+                    refreshed += 1 
+                    scrolled = 0
+                    time.sleep(3)
+
+            # Reset scrolled because new posts found
+            scrolled = 0
+            for btn in like_list:
+                btn.click()
                 liked += 1
                 # Like notification
-                if liked > step_liked:
-                    print('liked', liked)
+                if liked >= step_liked:
+                    print(t() + 'liked', liked)
                     step_liked += tmp
-
-                # errs -= 1
-                time.sleep(3)
+                time.sleep(delay)
 
         except selenium.common.exceptions.StaleElementReferenceException as e:
-            print('Stale elem', like_list.index(like_btn))
-            print(e)
+            # print('Stale element', like_list.index(btn))
+            # print(e)
             pass
+        
         except Exception as e:
-            print('Unexpected excep')
+            print(t() + 'Unexpected exception')
             print(e)
             errs += 1
             continue
 
-except KeyboardInterrupt as e:
-    print(e)
-    print('Ctrl-C was pressed...')
+except KeyboardInterrupt:
+    print(t() + 'Ctrl-C was pressed...')
 
 except Exception as e:
-    print(e)
+    print(t() + str(e))
 
-finally:
-    print('\nBot stopped')
-    print('to_like', to_like)
-    print('liked', liked)
-    print('errors', errs)
-    # bot.quit()
-    input('Press any key to contine...')
+
+print(t() + '\nDriver stopped')
+print('Target likes:'.ljust(14), to_like)
+print('Actual likes:'.ljust(14), liked)
+print('Refreshes:'.ljust(14), refreshed)
+print('Errors:'.ljust(14), errs)
+
+driver.quit() # Manually close browser if .quit() doesn't work
